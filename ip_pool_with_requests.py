1d4089bc7c613e9e9e8a981bf84efab62d356cf3
scotttyso <tyscott@cisco.com>
2023-04-24 14:31:15 -0400

    updating ip_pool with requests

--- ip_pool_with_requests.py
+++ ip_pool_with_requests.py
@@ -10,43 +10,58 @@ home = Path.home()
 
 api_auth = IntersightAuth(api_key_id=args.api_key_id, secret_key_filename=args.api_key_file)
 
-orgs = requests.get(f"https://{args.endpoint}/api/v1/organization/Organizations", auth=api_auth)
-orgs_json = orgs.json()
-for i in orgs_json['Results']:
-    if i['Name'] == 'default':
-        default_org_moid = i['Moid']
+def get_organization(org_name):
+    orgs = requests.get(f"https://{args.endpoint}/api/v1/organization/Organizations", auth=api_auth)
+    orgs_json = orgs.json()
+    for i in orgs_json['Results']:
+        if i['Name'] == org_name:
+            org_moid = i['Moid']
+    print(f'Organization {org_name} moid is: {org_moid}')
+    return org_moid
 
-print(f'default org moid is: {default_org_moid}')
-payload = {
-    'AssignmentOrder': 'default',
-    'Description': 'Demo Pool Creation',
-      "IpV4Blocks": [
-        {
-          "From": "198.18.24.101",
-          "ObjectType": "ippool.IpV4Block",
-          "Size": 27
+def create_ip_payload(org_moid):
+    payload = {
+        'AssignmentOrder': 'default',
+        'Description': 'Demo Pool Creation',
+        "IpV4Blocks": [
+            {
+            "From": "198.18.24.101",
+            "ObjectType": "ippool.IpV4Block",
+            "Size": 27
+            }
+        ],
+        "IpV4Config": {
+            "Gateway": "198.18.24.1",
+            "Netmask": "255.255.255.0",
+            "ObjectType": "ippool.IpV4Config",
+            "PrimaryDns": "10.101.128.15",
+            "SecondaryDns": "10.101.128.16"
+        },
+        "Name": "demo-198-18-24-0",
+        "ObjectType": "ippool.Pool",
+        "Organization": {
+            "Moid": org_moid,
+            "ObjectType": "organization.Organization"
         }
-      ],
-      "IpV4Config": {
-        "Gateway": "198.18.24.1",
-        "Netmask": "255.255.255.0",
-        "ObjectType": "ippool.IpV4Config",
-        "PrimaryDns": "10.101.128.15",
-        "SecondaryDns": "10.101.128.16"
-      },
-      "Name": "demo-198-18-24-0",
-      "ObjectType": "ippool.Pool",
-      "Organization": {
-        "Moid": default_org_moid,
-        "ObjectType": "organization.Organization"
-      }
-}
+    }
+    return payload
 
-pool = requests.post(f"https://{args.endpoint}/api/v1/ippool/Pools", auth=api_auth, json=payload)
+def create_ip_pool(args, payload):
+    pool = requests.post(f"https://{args.endpoint}/api/v1/ippool/Pools", auth=api_auth, json=payload)
+    return pool
 
-print(f'pool status is {pool}')
-if pool.json():
-    pool_json = pool.json()
-    pool_moid = pool_json['Moid']
-    print(f'pool demo-198-18-24-0 moid is:{pool_moid}')
-exit()
+
+def main():
+    org_moid = get_organization('default')
+    payload  = create_ip_payload(org_moid=org_moid)
+    pool_results = create_ip_pool(org_moid=org_moid, payload=payload)
+
+    print(f'pool status is {pool_results}')
+    if pool_results.json():
+        pool_json = pool_results.json()
+        pool_moid = pool_json['Moid']
+        print(f'pool demo-198-18-24-0 moid is:{pool_moid}')
+    exit()
+
+if __name__ == '__main__':
+    main()
